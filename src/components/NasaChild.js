f85b41660094d86330ad3ebfc168132d28c8c01f
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-14 16:18:33 -0400

    added NasaChild component function

--- src/components/NasaChild.js
+++ src/components/NasaChild.js
@@ -1,1 +1,15 @@
 import React from "react"
+
+const NasaChild = (props) => {
+    return (
+        <div>
+            <h1 className="title">{props.title}</h1>
+            <img src={props.link} />
+            <p className="date">{props.date}</p>
+            <p className="desc">{props.explanation}</p>
+        </div>
+    )
+}
+
+
+export default NasaChild;
