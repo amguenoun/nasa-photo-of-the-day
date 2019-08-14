a1465d66ea954e68ab58a3db2cd9d3f70151db53
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-14 16:26:47 -0400

    component functions are complete and props have been connected correctly

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import NasaParent from "./components/NasaParent"
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
       <NasaParent />
     </div>
   );
