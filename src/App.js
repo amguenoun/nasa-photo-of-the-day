5c5b7f16b80f1f751cf03312f38669acdb70205d
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-15 16:55:54 -0400

    added a menu to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import NasaParent from "./components/NasaParent"
+import NasaParent from "./components/NasaParent";
+import MenuBasic from './components/NavMenu';
 
 function App() {
   return (
     <div className="App">
+      <MenuBasic />
       <NasaParent />
     </div>
   );
