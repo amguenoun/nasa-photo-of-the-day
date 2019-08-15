dd559e8f7f84ad67f34993df16b90c43b268ced2
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-15 17:24:11 -0400

    added footer to the react app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import NasaParent from "./components/NasaParent";
 import MenuBasic from './components/NavMenu';
-
+import Footer from "./components/Footer"
 function App() {
   return (
     <div className="App">
       <MenuBasic />
       <NasaParent />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="foot">All information came from the Nasa API.</footer>
+    );
+}
+
+export default Footer;
