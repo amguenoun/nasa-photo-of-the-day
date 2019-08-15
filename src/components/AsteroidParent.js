d8c686e6b66a65e2ad7d59d23d4f7a5f3c676fbe
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-15 17:24:42 -0400

    added ternary operator to asteroidParent component function return

--- src/components/AsteroidParent.js
+++ src/components/AsteroidParent.js
@@ -10,15 +10,23 @@ const AsteroidParent = (props) => {
             .then(response => setList(response.data.near_earth_objects[date]))
             .catch(error => console.log("Error", error));
     }, [date])
+
+
+
     return (
-        <div className="astList">
-            <h1>List of Near Miss Asteroids {date}</h1>
-            <ul>
-                {list.map(item => {
-                    return <AsteroidChild key={item.id} id={item.id} distance={item.close_approach_data[0].miss_distance.kilometers}
-                        speed={item.close_approach_data[0].relative_velocity.kilometers_per_second} time={item.close_approach_data[0].close_approach_date_full.split(" ")[1]} />
-                })}
-            </ul>
+        <div>
+            {(!props.date) ? (<h3>Loading...</h3>) :
+                (<div className="astList">
+                    <h1>List of Near Miss Asteroids {date}</h1>
+                    <ul>
+                        {list.map(item => {
+                            return <AsteroidChild key={item.id} id={item.id} distance={item.close_approach_data[0].miss_distance.kilometers}
+                                speed={item.close_approach_data[0].relative_velocity.kilometers_per_second} time={item.close_approach_data[0].close_approach_date_full.split(" ")[1]} />
+                        })}
+                    </ul>
+                </div>
+                )
+            }
         </div>
     )
 }
