59190d8a54b666264362c4dae315cd1e4889d61a
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-15 17:46:19 -0400

    added material UI and a datePicker

--- src/components/NasaParent.js
+++ src/components/NasaParent.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"
 import AsteroidParent from "./AsteroidParent"
 import NasaCard from "./NasaCard";
+import DatePicker from "./DatePicker";
+
 const NasaParent = () => {
     const [nasaData, setNasaData] = useState({});
+    const [date, setDate] = useState(nasaData.date);
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY `)
             .then(response => {
@@ -13,12 +16,14 @@ const NasaParent = () => {
                 console.log("Error", error);
             })
     }, []);
+
     return (
         <div className="nasaParent">
             <h1>Nasa Astronomy Picture of the Day</h1>
-            <NasaCard title={nasaData.title} date={nasaData.date}
+            <DatePicker />
+            <NasaCard title={nasaData.title} date={date}
                 explanation={nasaData.explanation} link={nasaData.url} />
-            <AsteroidParent date={nasaData.date} />
+            <AsteroidParent date={date} />
         </div>
     )
 }
